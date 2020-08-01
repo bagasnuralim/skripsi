4ffacd0e66021f73104d0b21db50b46ada97fa31
bagasnuralim <bagasnuralim@gmail.com>
2020-08-01 20:51:10 +0700

    import jalan

--- uploader.php
+++ uploader.php
@@ -1,39 +1,53 @@
 <?php
-include_once('excel_reader2.php');
+include_once('excel_reader2.php'); 
 include_once('kondb.php');
 
-$target = basename($_FILES['data_peminjaman']['name']);
-move_uploaded_file($_FILES['data_peminjaman']['tmp_name'],$target);
+$target = basename($_FILES['data_karyawan']['name']);
+move_uploaded_file($_FILES['data_karyawan']['tmp_name'],$target);
 
-//permission agar file terbaca
-chmod($_FILES['data_peminjaman']['name'],0777);
-
-//mengisi isi file xls
-$data = new Spreadsheet_Excel_Reader($_FILES['data_peminjaman']['name'],false);
-$jumlah_baris = $data->rowcount($sheet_index=0);
-
-function format_date($date){
-    $date_ex = explode("/", $date);
-    return $date_ex[2]."-".$date_ex[1]."-".$date_ex[0];
+define("MIN_DATES_DIFF", 25569);
+ 
+// Numbers of second in a day:
+define("SEC_IN_DAY", 86400);
+ 
+function excel2timestamp($excelDate)
+{
+   if ($excelDate <= MIN_DATES_DIFF)
+      return 0;
+ 
+   return  ($excelDate - MIN_DATES_DIFF) * SEC_IN_DAY;
 }
 
-$success =0;
-for($i=2; $i<=$jumlah_baris; $i++){
-    $tanggal    = format_date($data->val($i, 1)); 
-    $buku       = $data->val($i, 2);
-    //$date = str_replace("/", "-", $tanggal);
-    //$dates = date("Y-m-j", strtotime($date));
+//permission agar file terbaca
+chmod($_FILES['data_karyawan']['name'],0777);
 
+//mengisi isi file xls
+$data = new Spreadsheet_Excel_Reader($_FILES['data_karyawan']['name'],false);
+$jumlah_baris = $data->rowcount($sheet_index=0);
+
+
+$success =0;
+
+for($i=2; $i<=$jumlah_baris; $i++){
+    $date        = $data->val($i, 1); 
+    $newformat  = excel2timestamp($date);
+    $tanggal = date('Y-m-d',$newformat);
+    $buku       = $data->val($i, 2);
+    //date('Y-m-d',$akses);
+    
     if($buku != ""){
-        mysqli_querry($connect, "INSERT INTO transaksi VALUES ('',
+        mysqli_query($connect, "INSERT INTO transaksi VALUES ('',
         '$tanggal','$buku')");
+        //,'$tanggal'
         $success++;
     }
 }
-unlink($_FILES['data_peminjaman']['name']);
+unlink($_FILES['data_karyawan']['name']);
 if($success >0){
-    header("location:index.php?menu=data_transaksi&pesan_success=Data berhasil disimpan");
+    echo "berhasil gas";
+    //header("location:index.php?upload=succes");
 }else{
-    header("location:index.php?menu=data_transaksi&pesan_success=Data gagal disimpan");
+    echo "gagal gas";
+    //header("location:index.php?upload=gagal");
 }
 ?>
